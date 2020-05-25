a145630d76ee42724ffc0b0c67fc57a5e7a2a173
“Ahmet <dbademir@gmail.com>
2020-05-25 20:06:34 +0300

    last commit for env

--- 02-environment/environment_variables.go
+++ 02-environment/environment_variables.go
@@ -6,7 +6,19 @@ import (
 )
 
 func main() {
+	uHome := os.Getenv("HOME")
+	uGoRoot := os.Getenv("GOROOT")
+	uGoPath := os.Getenv("GOPATH")
+	fmt.Println("Home:" + uHome)
+	fmt.Println("Go Root:" + uGoRoot)
+	fmt.Println("Go Path:" + uGoPath)
+
+}
+
+/*
+func main() {
 	for _, env := range os.Environ() {
 		fmt.Println(env)
 	}
 }
+*/
